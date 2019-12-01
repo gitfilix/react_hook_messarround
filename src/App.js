f5837237a25e2a6d1c9c6392e7fbaad5806fff8a
filiks <runlevel07@gmail.com>
2019-12-01 17:51:31 +0100

    hook setState v1

--- src/App.js
+++ src/App.js
@@ -1,84 +1,18 @@
 import React,{ useState, useEffect } from 'react';
 
-import Post from './post';
+const App = () => {
 
+  let [count, setCount] = useState(0);
 
-const App = ({initialCount}) => {
-
-
-  let [count,setCount] = useState(initialCount);
-  let [posts,setPosts] = useState([
-    {
-      name:'Super awesome hooks',
-      body:'Everything is awesome when your are part of a team'
-    }
-  ])
-
-
-  const addOne = () =>{
-    setCount( prevCount => { 
-      return prevCount + 1 
-    })
+  const addOne = () => {
+    setCount(count +1)
   }
-
-
-  const restOne = () =>{
-    setCount( prevCount => { 
-      return prevCount - 1 
-    })
-  }
-
-  const addOnePost = () => {
-    let newpost =   {
-      name:'Super awesome hooks 2',
-      body:'Everything is awesome when your are part of a team 2'
-    }
-
-    setPosts([
-      ...posts,
-      newpost
-    ])
-  }
-
-
-  useEffect(()=>{
-    // console.log(count)
-
-    return () => {
-      console.log('use effect count')
-    }
-
-  },[count])
-
-  // useEffect(()=>{
-  //   console.log('POSTS')
-  // },[posts])
-
-
-  // useEffect(()=>{
-  //   console.log('MOUNTED')
-  // },[])
-
-  const removePost = () => {
-    setPosts([])
-  }
-
 
   return (
     <>
+      <h1>Fancy Counter hook</h1>
       <h3>Count: {count}</h3>
-      <button onClick={addOne}>Add one +</button>
-      <button onClick={restOne}>Rest one -</button>
-      <button onClick={()=> setCount(initialCount)}>Reset</button>
-
-
-      { posts.map((item,i)=>(
-          <Post item={item} key={i}/>
-      ))}
-
-      <button onClick={addOnePost}>Add one more post</button>
-      <button onClick={removePost}>REMOVE POSTS</button>
-      
+      <button onClick={addOne}>Add 1 +</button>
     </>
   )
 }
