1f8fe72593f9b8d6e8307f7860e83ef4d0907b96
filiks <runlevel07@gmail.com>
2019-12-01 19:27:41 +0100

    hooks with useState another post

--- src/App.js
+++ src/App.js
@@ -2,19 +2,43 @@ import React,{ useState, useEffect } from 'react';
 
 const App = ({initialCount}) => {
 
+  // get inital value from props (index.js)
   let [count, setCount] = useState(initialCount);
+
+  let [posts, setPosts] = useState([
+    {
+      name: 'hanky hooks',
+      body: 'some people find hook fancy - I am not hooket by now'
+    },
+    {
+      name: 'barbyr baynes',
+      body: 'some people say we shall use it... so letz try out that shit...'
+    }
+  ])
+
 
   const addOne = () => {
     setCount( prevCount => {
       return prevCount +1
     })
   }
+
   const reduceOne = () => {
     setCount( prevCount => {
       return prevCount -1
     })
   }
 
+  const addOnePost = () => {
+    let anotherPost = {
+      name: 'holly hicks',
+      body: 'holy shit, we have another Post trolling here...'
+    }
+    setPosts([
+      ...posts,
+      anotherPost
+    ])
+  }
 
 
   return (
@@ -23,6 +47,18 @@ const App = ({initialCount}) => {
       <h3>Count: {count}</h3>
       <button onClick={addOne}>Add 1 +</button>
       <button onClick={reduceOne}>reduce 1 -</button>
+      <button onClick={()=> setCount(initialCount)}>Reset</button>
+      <hr />
+      <h3>Posts</h3>
+      { posts.map((item, i) => (
+        <div key={i}>
+          <div>Name: {item.name}</div>
+          <div>Body: {item.body}</div>
+          <br />
+        </div>
+      ))}
+      <button onClick={addOnePost}>Add posts</button>
+
     </>
   )
 }
