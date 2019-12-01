687cc3612577836a339fb42a69ba1330df2b9452
filiks <runlevel07@gmail.com>
2019-12-01 18:08:56 +0100

    hooks: inital count from props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React,{ useState, useEffect } from 'react';
 
-const App = () => {
+const App = ({initialCount}) => {
 
-  let [count, setCount] = useState(0);
+  let [count, setCount] = useState(initialCount);
 
   const addOne = () => {
-    setCount(count +1)
+    setCount( prevCount => {
+      return prevCount +1
+    })
   }
+  const reduceOne = () => {
+    setCount( prevCount => {
+      return prevCount -1
+    })
+  }
+
+
 
   return (
     <>
       <h1>Fancy Counter hook</h1>
       <h3>Count: {count}</h3>
       <button onClick={addOne}>Add 1 +</button>
+      <button onClick={reduceOne}>reduce 1 -</button>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App initialCount={1} />, document.getElementById('root'));
